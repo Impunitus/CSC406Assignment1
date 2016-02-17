5577c1d854969071835074fb1321594012629c2c
Caleb Butcher <butcher.caleb@gmail.com>
2016-02-17 16:02:32 -0500

    x

--- src/graphs/graphAlgs/matching/GreedyUnweightedMatchingStatic.java
+++ src/graphs/graphAlgs/matching/GreedyUnweightedMatchingStatic.java
@@ -16,9 +16,9 @@ public class GreedyUnweightedMatchingStatic extends MatchingAlgorithm
 {
     // decoration that indicates whether or not a Vertex has been added to the MST
     public static final Object VERTEX_STATUS = new Object();
-    // decoration that indicates whether or not an Edge has been added to the MST
-    public static final Object EDGE_STATUS = new Object();
-    
+    public static final Object VERTEX_DEGREE = new Object();
+    public static final Object VERTEX_NAME = new Object();
+
 	// Values for STATUS decorations
     public static final Object MATCHED = new Object();
     public static final Object FREE = new Object();
@@ -29,21 +29,15 @@ public class GreedyUnweightedMatchingStatic extends MatchingAlgorithm
 
         // This heap will contain all the edges in the graph
         // We will examine the edges from the least-costly to the most-costly
-        PriorityQueue<Edge<Double>> heap = new PriorityQueue<Edge<Double>>(11, new EdgeComparator());
+        PriorityQueue<Vertex<Double>> heap = new PriorityQueue<Vertex<Double>>(230, new VertexDegreeComparator());
 
-        // decorate all vertices as NOT USED  
+        // decorate all vertices as FREE
         for ( Vertex v : g.vertices() ) {
-            v.put ( VERTEX_STATUS, FREE );          
+            v.put ( VERTEX_STATUS, FREE );
         }
 
-        /* unsure if needed in this
-        // initialize all the edges of the graph as being NOT USED
-        for ( Edge e : g.edges() )
-            e.put ( EDGE_STATUS, NOT_USED ); 
-        */
-        
-        // If graph is empty, return immediately 
-        if ( g.numVertices() == 0 )  
+        // If graph is empty, return immediately
+        if ( g.numVertices() == 0 )
         	return;
     }
 
@@ -51,23 +45,23 @@ public class GreedyUnweightedMatchingStatic extends MatchingAlgorithm
     {
         drawNodes ( screen, testGraph, minDimension );
         drawEdges ( screen, testGraph, minDimension );
-    }   
+    }
 
     public void drawNodes ( Graphics screen, AdjacencyListGraph<Point2D.Double,Double> graph, int minDimension )
-    {     
+    {
         super.drawNodes(  screen, graph, minDimension );
 
         // label each vertex in the graph with its degree on the output screen
         for ( Vertex u : graph.vertices() )
         {
-            Point2D p = (Point2D) u.element(); 
+            Point2D p = (Point2D) u.element();
 
             double xPos = p.getX()  *  minDimension;
             double yPos = p.getY()  *  minDimension;
 
-//                         screen.drawString( (u.get(DEGREE)) + "", 
-//                                 (int)(xPos + 1.5*DOTRADIUS ), 
-//                                 (int)(yPos + 1.5*DOTRADIUS ) );  
+//                         screen.drawString( (u.get(DEGREE)) + "",
+//                                 (int)(xPos + 1.5*DOTRADIUS ),
+//                                 (int)(yPos + 1.5*DOTRADIUS ) );
         }
-    }	
+    }
 }
